ac29f76b208721d6efdf76e0969835c86b578c25
Wojciech Balon <balonw@gmail.com>
2023-01-10 21:46:39 +0100

    Auto stash before checking out "HEAD"

--- db/migrate/20230103084018_create_posts.rb
+++ db/migrate/20230103084018_create_posts.rb
@@ -1,9 +1,9 @@
 class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
-      t.string : title
-      t.text : description
-      t.datetime : published_at
+      t.string :title
+      t.text :description
+      t.datetime :published_at
       t.index_authors_on_email
     end
   end
